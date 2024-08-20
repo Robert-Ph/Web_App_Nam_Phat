a125b2482d9538ed3e54754e31939ab4844a446a
anhphuong012 <123961274+anhphuong012@users.noreply.github.com>
2024-08-20 20:42:30 +0700

    UI trang danh sach hoa don va chi tiet don hang

--- fontend/src/pages/InvoicePage/InvoicePaee.tsx
+++ fontend/src/pages/InvoicePage/InvoicePaee.tsx
@@ -2,14 +2,47 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import FilterListIcon from "@mui/icons-material/FilterList";
-import TuneIcon from "@mui/icons-material/Tune";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import "./listOrder.css";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import "../ListOrderPage/listOrder.css";
 import { order } from "../../model/person.model";
-const ListOrderPage = () => {
+
+const InvoicePage = () => {
   const [products, setProducts] = useState<order[]>([
     {
       id: "521345",
+      name: "Công ty TNHN XD Nguyễn Hoàng Tiến Phát Hoàng Thanh Hải Đăng Kiểm",
+      date: "31/12/2024",
+      price: "1 200 000 000 000 VNĐ",
+      isPay: "Chưa Thanh Toán",
+      status: "Chưa giao",
+    },
+    {
+      id: "123",
+      name: "Công ty TNHN XD Nguyễn Hoàng Tiến Phát Hoàng Thanh Hải Đăng Kiểm",
+      date: "31/12/2024",
+      price: "1 200 000 000 000 VNĐ",
+      isPay: "Chưa Thanh Toán",
+      status: "Chưa giao",
+    },
+    {
+      id: "4567",
+      name: "Công ty TNHN XD Nguyễn Hoàng Tiến Phát Hoàng Thanh Hải Đăng Kiểm",
+      date: "31/12/2024",
+      price: "1 200 000 000 000 VNĐ",
+      isPay: "Chưa Thanh Toán",
+      status: "Chưa giao",
+    },
+    {
+      id: "5213145",
+      name: "Công ty TNHN XD Nguyễn Hoàng Tiến Phát Hoàng Thanh Hải Đăng Kiểm",
+      date: "31/12/2024",
+      price: "1 200 000 000 000 VNĐ",
+      isPay: "Chưa Thanh Toán",
+      status: "Chưa giao",
+    },
+    {
+      id: "521321345",
       name: "Công ty TNHN XD Nguyễn Hoàng Tiến Phát Hoàng Thanh Hải Đăng Kiểm",
       date: "31/12/2024",
       price: "1 200 000 000 000 VNĐ",
@@ -20,7 +53,7 @@ const ListOrderPage = () => {
   return (
     <div>
       <div className="main-body">
-        <h3>Danh sách đơn hàng</h3>
+        <h3>Danh sách hóa đơn</h3>
         <div style={{ marginBottom: "10px" }}>
           <div
             className="d-flex justify-space-bettwen "
@@ -46,7 +79,7 @@ const ListOrderPage = () => {
               </Box>
             </div>
 
-            <div style={{ position: "relative" }}>
+            {/* <div style={{ position: "relative" }}>
               <select className="filter-select">
                 <option value="all">Tất cả</option>
                 <option value="newest">Mới nhất</option>
@@ -59,11 +92,11 @@ const ListOrderPage = () => {
               <i className="icon-filter">
                 <TuneIcon></TuneIcon>
               </i>
-            </div>
+            </div> */}
           </div>
         </div>
         <div style={{ padding: "20px" }}>
-          <div className="table-container">
+          <div className="table-more">
             <table style={{ width: "100%", borderCollapse: "collapse" }}>
               <thead>
                 <tr className="color-blue header-table text-left border-header-table">
@@ -72,23 +105,21 @@ const ListOrderPage = () => {
                   </th>
                   <th
                     className="pb-7 font-w-500"
-                    style={{ width: "25%", paddingRight: "10px" }}
+                    style={{ width: "8%", paddingRight: "10px" }}
+                  >
+                    Mã đơn hàng
+                  </th>
+                  <th
+                    className="pb-7 font-w-500"
+                    style={{ width: "20%", paddingRight: "10px" }}
                   >
                     Tên khách hàng
                   </th>
-                  <th className="pb-7 font-w-500" style={{ width: "12%" }}>
-                    Ngày
+                  <th className="pb-7 font-w-500" style={{ width: "10%" }}>
+                    Ngày tạo
                   </th>
-                  <th className="pb-7 font-w-500" style={{ width: "20%" }}>
-                    Giá (vnđ)
-                  </th>
-                  <th className="pb-7 font-w-500" style={{ width: "15%" }}>
-                    Thanh toán
-                  </th>
-                  <th className="pb-7 font-w-500" style={{ width: "12%" }}>
-                    Trạng thái
-                  </th>
-                  <th className="pb-7 font-w-500" style={{ width: "3%" }}></th>
+
+                  <th className="pb-7 font-w-500" style={{ width: "5%" }}></th>
                 </tr>
               </thead>
               <tbody className="border-header-table">
@@ -97,8 +128,11 @@ const ListOrderPage = () => {
                     <td className="pb-7 pt-7 font-size-small td-table font-w-500 ">
                       {product.id}
                     </td>
+                    <td className="pb-7 pt-7 font-size-small font-w-500 ">
+                      {product.id || "-"}
+                    </td>
                     <td
-                      className="pb-7 pt-7 font-size-small font-w-500 "
+                      className="pb-7 pt-7 font-size-small td-table font-w-500"
                       style={{ paddingRight: "20px" }}
                     >
                       {product.name || "-"}
@@ -106,28 +140,24 @@ const ListOrderPage = () => {
                     <td className="pb-7 pt-7 font-size-small td-table font-w-500">
                       {product.date || "-"}
                     </td>
+
                     <td className="pb-7 pt-7 font-size-small td-table font-w-500">
-                      {product.price || "-"}
-                    </td>
-                    <td className="pb-7 pt-7 font-size-small td-table font-w-500">
-                      {product.isPay || "-"}
-                    </td>
-                    <td className="pb-7 pt-7 font-size-small td-table font-w-500">
-                      {product.status || "-"}
-                    </td>
-                    <td className="pb-7 pt-7 font-size-small td-table font-w-500">
-                      <button className="btn-more">
-                        <MoreHorizIcon></MoreHorizIcon>
-                      </button>
+                      <button className="btn btn-primary">Xuất File</button>
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
+          <div className="pagination">
+            <Stack spacing={2}>
+              <Pagination count={10} color="primary" />
+            </Stack>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-export default ListOrderPage;
+
+export default InvoicePage;
